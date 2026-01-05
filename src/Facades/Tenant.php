568dcbed6ee7c28aa4cd4928990667c2058c99c8
uendelsilveira <uendel.silveira@gmail.com>
2026-01-05 10:08:59 -0300

    Update namespace in Tenant facade and add method documentation

--- src/Facades/Tenant.php
+++ src/Facades/Tenant.php
@@ -6,10 +6,19 @@
 	 Created at: 05/01/26
 	*/
 
-namespace Uendel\LaravelTenantCore\Facades;
+namespace UendelSilveira\TenantCore\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \UendelSilveira\TenantCore\Contracts\TenantContract|null current()
+ * @method static string|int|null key()
+ * @method static string|null slug()
+ * @method static bool isCentral()
+ * @method static bool isTenant()
+ *
+ * @see \UendelSilveira\TenantCore\TenantManager
+ */
 class Tenant extends Facade
 {
     protected static function getFacadeAccessor(): string { return 'tenant'; }
